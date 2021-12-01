ffe2001671be15159ca7774f3e56e5eefb01e9c0
soblab <soblab@Whit209-12.it.farmingdale.edu>
2021-12-01 13:21:22 -0500

    Problem two framework

--- src/main/java/Lab14/ParenMatch.java
+++ src/main/java/Lab14/ParenMatch.java
@@ -27,8 +27,22 @@ for(int i = 0 ; i < line.length() && correctNum ; i++){
     }
 }
 // print the results
-if(s.isEmpty() && correctNum == true)
+if(s.isEmpty() && correctNum == true){
    System.out.println("There is the correct number of parenthesis in the expression");
+   Stack number = new Stack();
+   Stack operators = new Stack();
+   for(int j = 0 ; j < line.length() ; j++){
+        char o = line.charAt(j);
+        if(o == '%' || o ==  '/' || o == '*' || o == '+' || o == '-' )
+            operators.push(o);
+        else if(o == ')')
+            System.out.println("");
+            //evaluate expression with 2 pops from number and one pop from operator
+        else
+            number.push(line.charAt(j));
+   }
+    System.out.println(number.pop());
+}
 else if(s.isEmpty() == false)
    System.out.println("Too many left parentheses");
  }
